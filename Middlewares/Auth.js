dd3937a62f99d369cb4bb2b5817636565e4fcf77
ajaysingh-githubcode <shareajay84@gmail.com>
2024-10-01 08:25:51 +0530

    first commit

---
+++ Middlewares/Auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import User from "../Models/user.model.js";
+
+let auth = async (req, res, next) => {
+  let token = req?.cookies?.Token;
+  try {
+    if (!token) {
+      return res.send({ result: false, message: "Not Authenticated " });
+    }
+
+    let TokenData = jwt.verify(token, process.env.SECRET_KEY);
+
+    if (TokenData) {
+      let userData = await User.findOne({ email: TokenData.email });
+      req.user = userData;
+      next();
+    } else {
+      return res.send({ result: false, message: "Not Authenticated " });
+    }
+  } catch (err) {
+    res.send({ result: false, message: err.message });
+  }
+};
+
+export default auth;
---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dbConnect from "./db.js";
+import userRouter from "./Routes/user.route.js";
+import cookieParser from "cookie-parser";
+import 'dotenv/config';
+import cors from 'cors';
+
+let server = express();
+
+let PORT = process.env.PORT || 8080;
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cookieParser());
+
+server.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+server.use("/users", userRouter);
+
+dbConnect()
+  .then(() => {
+    console.log(" DataBase connected ");
+    server.listen(PORT, () => {
+      console.log(" Sever is running on  ", `http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
